0b7f56998d7b218ac79d88bd745b1d10021bccf2
RichardZhangCS <richardzhang1303@gmail.com>
2021-10-23 01:25:31 -0400

    Added modal support to Awards sections

--- client/src/pages/Dashboards.js
+++ client/src/pages/Dashboards.js
@@ -1,6 +1,9 @@
 import { ListGroup } from "react-bootstrap";
 import { Container, Row, Col } from "react-bootstrap";
 import { Badge, Button } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 
 function CustomerDashboard(props) {
   return (
@@ -49,95 +52,128 @@ function CustomerDashboard(props) {
 }
 
 function BusinessDashboard() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <Container className="pt-4">
-      <h3 className="text-center">Business Dashboard</h3>
-      <Row>
-        <Col md>
-          <ListGroup>
-            <h4>Customers</h4>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Mary Joe</div>
-                ID: 223892
-              </div>
-              <Badge variant="primary" pill>
-                475
-              </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Hope Wild</div>
-                ID: 223892
-              </div>
-              <Badge variant="primary" pill>
-                28
-              </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Customer 3</div>
-                ID: 223892
-              </div>
-              <Badge variant="primary" pill>
-                30
-              </Badge>
-            </ListGroup.Item>
-          </ListGroup>
-          <Button className="mt-2 float-right">Clear All</Button>
-        </Col>
-        <Col md>
-          <ListGroup>
-            <h4>Awards</h4>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Andrew's Awesome ML Co.</div>
-              </div>
-              <Badge bg="success" pill>
-                475
-              </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Mum and Dad's Shop</div>
-              </div>
-              <Badge bg="success" pill>
-                28
-              </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-              as="li"
-              className="d-flex justify-content-between align-items-start"
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">Small Business 3</div>
-              </div>
-              <Badge bg="success" pill>
-                30
-              </Badge>
-            </ListGroup.Item>
-          </ListGroup>
-          <Button variant="success" className="mt-2 float-right">
-            Add New Award
+    <>
+      <Container className="pt-4">
+        <h3 className="text-center">Business Dashboard</h3>
+        <Row>
+          <Col md>
+            <ListGroup>
+              <h4>Customers</h4>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Mary Joe</div>
+                  ID: 223892
+                </div>
+                <Badge variant="primary" pill>
+                  475
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Hope Wild</div>
+                  ID: 223892
+                </div>
+                <Badge variant="primary" pill>
+                  28
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start"
+              >
+                <div className="ms-2 me-auto award-item">
+                  <div className="fw-bold">Customer 3</div>
+                  ID: 223892
+                </div>
+                <Badge variant="primary" pill>
+                  30
+                </Badge>
+              </ListGroup.Item>
+            </ListGroup>
+            <Button className="mt-2 float-right">Clear All</Button>
+          </Col>
+          <Col md>
+            <ListGroup>
+              <h4>Awards</h4>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start award-item"
+                onClick={handleShow}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Andrew's Awesome ML Co.</div>
+                </div>
+                <Badge bg="success" pill>
+                  475
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start award-item"
+                onClick={handleShow}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Mum and Dad's Shop</div>
+                </div>
+                <Badge bg="success" pill>
+                  28
+                </Badge>
+              </ListGroup.Item>
+              <ListGroup.Item
+                as="li"
+                className="d-flex justify-content-between align-items-start award-item"
+                onClick={handleShow}
+              >
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Small Business 3</div>
+                </div>
+                <Badge bg="success" pill>
+                  30
+                </Badge>
+              </ListGroup.Item>
+            </ListGroup>
+            <Button variant="success" className="mt-2 float-right">
+              Add New Award
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <ViewAwardModal show={show} handleClose={handleClose} />
+    </>
+  );
+}
+
+function ViewAwardModal(props) {
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.handleClose}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Add New Award</Modal.Title>
+      </Modal.Header>
+      <Form>
+        <Modal.Body></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.handleClose}>
+            Cancel
           </Button>
-        </Col>
-      </Row>
-    </Container>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
   );
 }
 
